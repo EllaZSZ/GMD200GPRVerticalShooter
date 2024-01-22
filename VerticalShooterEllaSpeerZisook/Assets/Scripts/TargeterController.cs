0ca14428565d469468930dfbfe48d06cb9f600a4
ella.speer-zisook <ella.speer-zisook@mymail.champlain.edu>
2024-01-22 12:49:50 -0500

    Added shell prefab, player, and targeter

---
+++ VerticalShooterEllaSpeerZisook/Assets/Scripts/TargeterController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargeterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+    }
+}
